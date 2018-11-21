13e3e59dfd1592504a76290edae9785973ee2f6e
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-11-21 02:25:34 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE characters(
+CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
   name TEXT,
-  motto TEXT,
   species TEXT,
+  motto TEXT,
   author_id INTEGER,
   series_id INTEGER
 );
