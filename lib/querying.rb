bb08ed302d7a50f2fe04c6300f160075186dfde2
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-11-21 02:20:07 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,11 +1,11 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   # "SELECT title, year FROM books WHERE series_id=1 ORDER BY year"
   <<-SQL
-  SELECT title, year 
-  FROM books 
-  WHERE series_id=1 
+  SELECT title, year
+  FROM books
+  WHERE series_id=1
   ORDER BY year
-SQL
+  SQL
 end
 
 def select_name_and_motto_of_char_with_longest_motto
